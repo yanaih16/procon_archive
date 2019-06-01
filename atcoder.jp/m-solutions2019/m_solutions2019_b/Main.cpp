ecde83fc4a1ba8650f04e66245b6da9148673b35
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-01 21:09:22 +0900

    [AC] m-solutions2019 m_solutions2019_b

---
+++ atcoder.jp/m-solutions2019/m_solutions2019_b/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int ans=0;
+    string s;
+    cin >>s;
+    for(int i=0;i<s.size();i++){
+        if(s[i] == 'x') ans ++;
+    }
+    cout <<(ans>=8 ? "NO" : "YES")<<endl;
+
+    return 0;
+}
