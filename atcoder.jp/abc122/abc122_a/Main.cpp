5384896daf78e000424fc949e1c6a5ef133c4fed
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-05 16:08:30 +0900

    [AC] abc122 abc122_a

---
+++ atcoder.jp/abc122/abc122_a/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    char s;
+    cin >>s;
+    switch(s){
+        case 'A': 
+            cout <<"T"<<endl;
+            break;
+        case 'C': 
+            cout <<"G"<<endl;
+            break;
+        case 'T': 
+            cout <<"A"<<endl;
+            break;
+        case 'G': 
+            cout <<"C"<<endl;
+    }
+
+    return 0;
+}
