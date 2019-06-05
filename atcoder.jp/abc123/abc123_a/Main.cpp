2d7230918009a77085cd5a1de0cd27f9532fd00b
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-05 15:59:16 +0900

    [AC] abc123 abc123_a

---
+++ atcoder.jp/abc123/abc123_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    int a,b,c,d,e,k;
+    cin >>a>>b>>c>>d>>e>>k;
+    cout <<(e-a<=k?"Yay!":":(")<<endl;
+
+    return 0;
+}
