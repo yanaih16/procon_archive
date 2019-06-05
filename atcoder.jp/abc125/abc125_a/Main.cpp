158b507f0b708beffeb781b22bf176d4b5047710
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-05 15:50:51 +0900

    [AC] abc125 abc125_a

---
+++ atcoder.jp/abc125/abc125_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    int a,b,t,ans;
+    cin >>a>>b>>t;
+    ans = t/a*b;
+    cout <<ans<<endl;
+
+    return 0;
+}
