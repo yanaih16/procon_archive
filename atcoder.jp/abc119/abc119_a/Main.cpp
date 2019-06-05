33c88c4523c5bd9b5ce698ce1d254f9a5306ec17
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-05 16:48:13 +0900

    [AC] abc119 abc119_a

---
+++ atcoder.jp/abc119/abc119_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    string s;
+    cin >>s;
+    if(4<stoi(s.substr(5,2)))cout <<"TBD"<<endl;
+    else cout <<"Heisei"<<endl;
+
+    return 0;
+}
