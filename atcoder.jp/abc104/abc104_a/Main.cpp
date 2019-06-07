6f9b5813939555fc55d76a57ef7fb0f7311db598
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-07 21:08:22 +0900

    [AC] abc104 abc104_a

---
+++ atcoder.jp/abc104/abc104_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n<1200)cout<<"ABC"<<endl;
+    else if(n<2800)cout<<"ARC"<<endl;
+    else cout<<"AGC"<<endl;
+    return 0;
+}
