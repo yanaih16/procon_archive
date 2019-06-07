0ce6005a1bdd52ee1224f9b3fec13cd172d91e7c
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-07 16:51:46 +0900

    [AC] abc112 abc112_a

---
+++ atcoder.jp/abc112/abc112_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    int n,a,b;
+    cin>>n>>a>>b;
+    if(n==1)cout<<"Hello World"<<endl;
+    else cout<<a+b<<endl;
+
+    return 0;
+}
