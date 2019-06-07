b8a9c8886455121ff8eef07003fcf86cc7b5613b
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-07 16:48:21 +0900

    [AC] abc114 abc114_a

---
+++ atcoder.jp/abc114/abc114_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    int x;
+    cin>>x;
+    if(x==3||x==5||x==7)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+    return 0;
+}
