775a60519357543c52262410cddf184da61524e5
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-07 16:46:00 +0900

    [AC] abc115 abc115_a

---
+++ atcoder.jp/abc115/abc115_a/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    int d;
+    cin >>d;
+    switch(d){
+        case 25:
+            cout<<"Christmas"<<endl;
+            break;
+        case 24:
+            cout<<"Christmas Eve"<<endl;
+            break;
+        case 23:
+            cout<<"Christmas Eve Eve"<<endl;
+            break;
+        case 22:
+            cout<<"Christmas Eve Eve Eve"<<endl;
+    }
+
+    return 0;
+}
