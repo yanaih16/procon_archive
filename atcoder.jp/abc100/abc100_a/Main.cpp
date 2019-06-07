39e69ef027bcc3a42923ad5f9070a7388adbcff6
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-07 21:25:04 +0900

    [AC] abc100 abc100_a

---
+++ atcoder.jp/abc100/abc100_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a>8||b>8)cout<<":("<<endl;
+    else cout<<"Yay!"<<endl;
+    return 0;
+}
