ad2a09222696b1644efa160f0bcb50ca7bcdc649
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-08 13:52:28 +0900

    [AC] abc094 abc094_a

---
+++ atcoder.jp/abc094/abc094_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    int a,b,x;
+    cin>>a>>b>>x;
+    if(a<=x&&a+b>=x)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    return 0;
+}
