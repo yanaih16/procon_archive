d892e6754f61e560db6eacd5a102b4167c2d823c
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-21 21:27:30 +0900

    [AC] abc075 abc075_a

---
+++ atcoder.jp/abc075/abc075_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b)cout<<c<<endl;
+    else if(a==c)cout<<b<<endl;
+    else cout<<a<<endl;
+    return 0;
+}
