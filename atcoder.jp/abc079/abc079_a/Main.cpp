1d580e43fd5f87e55b7931f093429d1254a6f0e1
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-21 20:57:40 +0900

    [AC] abc079 abc079_a

---
+++ atcoder.jp/abc079/abc079_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    string s;
+    cin>>s;
+    if(s[0]==s[1]&&s[1]==s[2]) cout<<"Yes"<<endl;
+    else if(s[1]==s[2]&&s[2]==s[3])cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}
