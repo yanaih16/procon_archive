adc8922a85062ae85727464e8635f79604a9182a
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-22 14:27:57 +0900

    [AC] abc063 abc063_a

---
+++ atcoder.jp/abc063/abc063_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a+b<10)cout<<a+b<<endl;
+    else cout<<"error"<<endl;
+    return 0;
+}
