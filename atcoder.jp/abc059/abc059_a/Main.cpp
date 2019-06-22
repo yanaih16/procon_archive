99baeeaccb6f9ec5e0d4554b1f735d15a5f8e0de
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-06-23 07:36:35 +0900

    [AC] abc059 abc059_a

---
+++ atcoder.jp/abc059/abc059_a/Main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    string a,b,c,ans;
+    cin>>a>>b>>c;
+    cout<<(char)(a[0]-32)<<(char)(b[0]-32)<<(char)(c[0]-32)<<endl;
+    return 0;
+}
