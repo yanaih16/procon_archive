d660c926d15a67481be1ee647d73f332a585e705
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-07-12 17:11:36 +0900

    [AC] abc049 abc049_a

---
+++ atcoder.jp/abc049/abc049_a/Main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    char s;
+    cin>>s;
+    if(s=='a'||s=='i'||s=='u'||s=='e'||s=='o') cout<<"vowel"<<endl;
+    else cout<<"consonant"<<endl;
+}
