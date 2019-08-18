6d8c910317ddafa55e0d21ccf8525e6c41a82543
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-08-18 22:10:05 +0900

    [AC] abc138 abc138_a

---
+++ atcoder.jp/abc138/abc138_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+    int a;
+    string s;
+    cin>>a>>s;
+    cout<<(a>=3200?s:"red")<<endl;
+    return 0;
+}
