937fa017d04f9f6a4cfda2c4ba6c9daf6dd4ba85
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2019-11-22 15:45:20 +0900

    [AC] abc042 abc042_a

---
+++ atcoder.jp/abc042/abc042_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+int main(){
+    i64 a,b,c;
+    cin>>a>>b>>c;
+    if(a==5&&b==7&&c==5) cout<<"YES"<<endl;
+    else if(a==7&&b==5&&c==5)cout<<"YES"<<endl;
+    else if(a==5&&b==5&&c==7)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    return 0;
+}
