b806ba3323721dea2639aacd8df5bbe660d04510
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2020-04-04 21:01:15 +0900

    [AC] abc161 abc161_a

---
+++ atcoder.jp/abc161/abc161_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    i64 a,b,c;
+    cin>>a>>b>>c;
+    swap(a,b);
+    swap(a,c);
+    cout<<a<<" "<<b<<" "<<c<<endl;
+
+    return 0;
+}
