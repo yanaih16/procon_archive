9ab7dc56bd5afb0077d75aa7ef1659d3d3988c94
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2022-01-30 21:01:57 +0900

    [AC] abc237 abc237_a

---
+++ atcoder.jp/abc237/abc237_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    i64 n;
+    cin>>n;
+    if(n<2147483648&&n>=-2147483648) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}
