494a15362e05b68e78085a153077d60c7dbc8070
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2022-09-17 21:00:58 +0900

    [AC] abc269 abc269_a

---
+++ atcoder.jp/abc269/abc269_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main() {
+    i64 a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    cout<<(a+b)*(c-d)<<endl;
+    cout<<"Takahashi"<<endl;
+
+    return 0;
+}
