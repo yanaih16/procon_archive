4630186dd3c07bb0faa45b393bd84bd4e780486e
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-01-28 22:27:58 +0900

    [AC] abc287 abc287_c

---
+++ atcoder.jp/abc287/abc287_c/Main.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+#define MAX_V 200000
+#define MAX_E 200000
+
+i64 par[MAX_V];
+i64 my_rank[MAX_V];
+
+void init_union_find(i64 n){
+    for(i64 i = 0; i < n; i++){
+        par[i] = i;
+        my_rank[i] = 0;
+    }
+}
+
+i64 find(i64 x){
+    if(par[x] == x){
+        return x;
+    } else {
+        return par[x] = find(par[x]);
+    }
+}
+
+void unite(i64 x, i64 y){
+    x = find(x);
+    y = find(y);
+    if(x == y) return;
+
+    if(my_rank[x] < my_rank[y]){
+        par[x] = y;
+    } else {
+        par[y] = x;
+        if(my_rank[x] == my_rank[y]) my_rank[x]++;
+    }
+}
+
+bool same(i64 x, i64 y){
+    return find(x) == find(y);
+}
+
+int main() {
+    i64 n, m;
+    cin>>n>>m;
+    init_union_find(n);
+    if(m != n - 1){
+        for(i64 i = 0; i < m; i++){
+            i64 s, t;
+            cin>>s>>t;
+        }
+        cout<<"No"<<endl;
+        return 0;
+    }
+    for(i64 i = 0; i < m; i++){
+        i64 s, t;
+        cin>>s>>t;
+        if(same(s, t)){
+            cout<<"No"<<endl;
+            return 0;
+        } else {
+            unite(s, t);
+        }
+    }
+    cout<<"Yes"<<endl;
+    return 0;
+}
