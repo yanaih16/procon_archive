c2562b9dee939812bc82052d5db62933b7b2a521
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-02-04 21:00:50 +0900

    [AC] abc288 abc288_a

---
+++ atcoder.jp/abc288/abc288_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main() {
+    i64 n;
+    cin>>n;
+    for(i64 i = 0; i < n; i++){
+        i64 a, b;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+
+    return 0;
+}
