fca02d1963adcb3ea3d4d5a3f4d16d47c3c9f211
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-05-20 21:01:57 +0900

    [AC] abc302 abc302_a

---
+++ atcoder.jp/abc302/abc302_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define MOD 998244353
+
+int main() {
+    i64 a, b;
+    cin >> a >> b;
+    cout << (a + b - 1) / b << endl;
+    return 0;
+}
