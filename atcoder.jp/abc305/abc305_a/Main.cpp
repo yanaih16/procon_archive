214405a4f763a2c9b679cec9cff72e0afecc6d51
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-06-10 21:02:35 +0900

    [AC] abc305 abc305_a

---
+++ atcoder.jp/abc305/abc305_a/Main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define rep(i, n) for (i64 i = 0; i < (i64)(n); i++)
+#define rep2(i, a, n) for (i64 i = (a); i < (i64)(n); i++)
+#define fi first
+#define se second
+#define MOD 998244353
+typedef pair<i64, i64> P;
+
+void Main() {
+    i64 n;
+    cin >> n;
+    i64 a = n / 5;
+    a *= 5;
+    i64 b = (n + 4) / 5;
+    b *= 5;
+    if (n - a < b - n)
+        cout << a << endl;
+    else
+        cout << b << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
