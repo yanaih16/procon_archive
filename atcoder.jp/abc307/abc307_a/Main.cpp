0df80f431f52b636d03216288ce027b6400c5337
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-06-24 21:02:43 +0900

    [AC] abc307 abc307_a

---
+++ atcoder.jp/abc307/abc307_a/Main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define rep(i, n) for (i64 i = 0; i < (i64)(n); i++)
+#define rep2(i, a, n) for (i64 i = (a); i < (i64)(n); i++)
+#define fi first
+#define se second
+#define MOD 998244353
+typedef pair<i64, i64> P;
+
+void Main() {
+    i64 n;
+    cin >> n;
+    i64 sum;
+    rep(i, n) {
+        sum = 0;
+        rep(i, 7) {
+            i64 a;
+            cin >> a;
+            sum += a;
+        }
+        cout << sum << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
