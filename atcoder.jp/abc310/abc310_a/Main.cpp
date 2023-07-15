4df97bfa64638f45a28f44238f129295913d8b08
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-07-15 21:02:17 +0900

    [AC] abc310 abc310_a

---
+++ atcoder.jp/abc310/abc310_a/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define rep(i, n) for (i64 i = 0; i < (i64)(n); i++)
+#define rep2(i, a, n) for (i64 i = (a); i < (i64)(n); i++)
+#define vsort(v) sort(v.begin(), v.end())
+#define vsortr(v) sort(v.begin(), v.end(), greater<i64>)
+#define fi first
+#define se second
+#define MOD 998244353
+typedef pair<i64, i64> P;
+
+void Main() {
+    i64 n, p, q;
+    cin >> n >> p >> q;
+    vector<i64> d(n);
+    for (auto& v : d) cin >> v;
+    vsort(d);
+    cout << (p < q + d[0] ? p : q + d[0]) << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
